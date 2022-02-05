47173af9430ee1bdb78786dc1b5166637210afd1
karl-adriano <97026510+karl-adriano@users.noreply.github.com>
2022-02-04 19:15:55 -0800

    Secured endpoints and merged to one file (#3)

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
 "use strict"
 
-var express = require("express")
-var app = express()
+var express = require("express");
+var app = express();
 
 app.listen(3000)
 console.log("Node.js Express server is running on port 3000...")
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  const allowedOrigins = ['https://editor.swagger.io', 'https://hoppscotch.io'];
+  const allowedOrigins = ["https://editor.swagger.io", "https://hoppscotch.io"];
   const origin = req.headers.origin;
       
   if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader("Access-Control-Allow-Origin", origin);
   }
       
   // Request methods you wish to allow eg: GET, POST, OPTIONS, PUT, PATCH, DELETE
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
   
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  
+  res.setHeader("Access-Control-Allow-Headers", ["Authorization", "X-Requested-With,content-type"]);
+
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader != "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  }
+
   // Pass to next layer of middleware
   next();
 });
   
 app.get("/v1/weather", get_weather)
 
-function get_weather(request, response) {
-    response.json( {
+function get_weather(req, res) {
+  if (req.token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkthcmwgQWRyaWFubyIsImlhdCI6MTUxNjIzOTAyMn0.STyNSCjMt9cKNL6YVfLRTBabPbzbW1FDRebDqTwC2-c") {
+    res.json({
       "coord": {
         "lon": -123.262,
         "lat": 44.5646
@@ -70,5 +79,31 @@ function get_weather(request, response) {
       "id": 5720727,
       "name": "Corvallis",
       "cod": 200
+    });
+  }
+  else {
+    res.sendStatus(401);
+  }  
+}
+
+app.get("/v1/hello", greet);
+
+function greet(req, res) {
+    if (req.token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkthcmwgQWRyaWFubyIsImlhdCI6MTUxNjIzOTAyMn0.STyNSCjMt9cKNL6YVfLRTBabPbzbW1FDRebDqTwC2-c") {
+      res.json({
+        "greeting": "Karl says Hello" 
+      });
+    }
+    else {
+      res.sendStatus(401);
+    }
+}
+
+app.post("/v1/auth", auth);
+
+function auth(req, res) {
+    res.json( {
+      "access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkthcmwgQWRyaWFubyIsImlhdCI6MTUxNjIzOTAyMn0.STyNSCjMt9cKNL6YVfLRTBabPbzbW1FDRebDqTwC2-c",
+      "expires": "2022-02-11T23:59:59.999-08:00"
     } )
 }
